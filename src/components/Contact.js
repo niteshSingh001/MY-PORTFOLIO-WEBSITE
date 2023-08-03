5f1f2900ca759a11899b4647a6448a82c46afda8
Nitesh Singh <niteshsinghm21@gmail.com>
2023-08-03 16:09:31 +0530

    updated Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,134 +1,76 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import contactImg from "../assets/img/contact-img.svg";
 import { Container, Row, Col } from "react-bootstrap";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
+  const form = useRef();
 
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "Application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
 
-    if (result.code === 200) {
-      setStatus({ success: true, message: "Mesage Sent Successfully" });
-    } else {
-      setStatus({
-        success: false,
-        message: "Something Went Wrong,Please try again Later.",
-      });
-    }
+    emailjs
+      .sendForm(
+        "service_tejaqo5",
+        "template_5czy8rr",
+        form.current,
+        "BP5Q0MfyjmBdeUL7f"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
   };
 
   return (
-    <div>
-      <section className="contact" id="connect">
-        <Container>
-          <Row className="align-items-center">
-            <Col md={6}>
-              <img src={contactImg} alt="Contact Us" />
-            </Col>
-            <Col md={6}>
-              <h2>Get In Touch</h2>
-              <form onSubmit={handleSubmit}>
-                <Row>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="text"
-                      value={formDetails.firstName}
-                      placeholder="First Name"
-                      onChange={(e) => {
-                        onFormUpdate("firstName", e.target.value);
-                      }}
-                    />
-                  </Col>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="text"
-                      value={formDetails.lastName}
-                      placeholder="Last Name"
-                      onChange={(e) => {
-                        onFormUpdate("lastName", e.target.value);
-                      }}
-                    />
-                  </Col>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="email"
-                      value={formDetails.email}
-                      placeholder="Email Address"
-                      onChange={(e) => {
-                        onFormUpdate("email", e.target.value);
-                      }}
-                    />
-                  </Col>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="tel"
-                      value={formDetails.phone}
-                      placeholder="Phone No."
-                      onChange={(e) => {
-                        onFormUpdate("phone", e.target.value);
-                      }}
-                    />
-                  </Col>
-                  <Col>
-                    <textarea
-                      rows="6"
-                      value={formDetails.message}
-                      placeholder="Message"
-                      onChange={(e) => {
-                        onFormUpdate("message", e.target.value);
-                      }}
-                    ></textarea>
-                    <button type="submit">
-                      <span>{buttonText}</span>
-                    </button>
-                  </Col>
-                  {status.message && (
-                    <Col>
-                      <p
-                        className={
-                          status.success === false ? "danger" : "success"
-                        }
-                      >
-                        {status.message}
-                      </p>
-                    </Col>
-                  )}
-                </Row>
-              </form>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </div>
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} alt="Contact Us" />
+          </Col>
+          <Col md={6}>
+            <h2>Get In Touch</h2>
+            <form onSubmit={sendEmail} ref={form}>
+              <Row>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    placeholder="FullName"
+                    name="name"
+                    required
+                  />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="email"
+                    placeholder="Email Address"
+                    name="email"
+                    required
+                  />
+                </Col>
+                <Col>
+                  <textarea
+                    rows="6"
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                  <button type="submit">
+                    <span>Send</span>
+                  </button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
 
